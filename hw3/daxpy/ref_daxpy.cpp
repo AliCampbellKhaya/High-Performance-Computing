110797bca479e1577cd51f728407bf14aa834523
Alasdair Campbell <alicampbellkhaya@gmail.com>
2026-04-20 17:01:06 -0700

    initial daxpy attempt

---
+++ hw3/daxpy/ref_daxpy.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+void daxpy(double a, const std::vector<double> &x, std::vector<double> &y)  {
+    int n = 512;
+    int ntrial = 5; // tbd
+
+    for (int i = 2; i <= 512; i++) {
+
+        for (int j = 0; j < ntrial; j++) {
+
+            
+
+        }
+    }
+    
+}
--- hw3/dgemm/ref_dgemm.cpp
+++ hw3/dgemm/ref_dgemm.cpp
@@ -1,3 +1,7 @@
 #include <vector>
 
-void dgemm(double a, const std::vector<std::vector<double>> &A, const std::vector<std::vector<double>> &B, double b, std::vector<std::vector<double>> &C)
+void dgemm(double a, const std::vector<std::vector<double>> &A, const std::vector<std::vector<double>> &B, double b, std::vector<std::vector<double>> &C) {
+    int ntrial = 5; // tbd
+
+    for (int n = 1)
+}
