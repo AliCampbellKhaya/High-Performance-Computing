b6e353ea5feb4ca5e7261dc511cd5298291feba5
Alasdair Campbell <alicampbellkhaya@gmail.com>
2026-04-22 09:07:28 -0700

    same hpp error

--- hw3/dgemm/ref_dgemm.hpp
+++ hw3/dgemm/ref_dgemm.hpp
@@ -2,6 +2,7 @@
 #define REF_DGEMM_HPP
 
 #include <vector>
+#include <stdexcept>
 
 void dgemm(double a, const std::vector<std::vector<double>> &A, const std::vector<std::vector<double>> &B,
             double b, std::vector<std::vector<double>> &C);
