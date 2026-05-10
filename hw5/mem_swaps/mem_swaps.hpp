8148739692ad20fa21d0de9e7358a9c5016d426f
Alasdair Campbell <alicampbellkhaya@gmail.com>
2026-05-09 17:30:44 -0700

    refactor to use std swap

--- hw5/mem_swaps/mem_swaps.hpp
+++ hw5/mem_swaps/mem_swaps.hpp
@@ -2,19 +2,12 @@
 #define MEM_SWAPS_HPP
 
 #include <vector>
-#include <iostream>
+#include <algorithm>
 
 void swapRows(std::vector<double> &matrix, int nRows, int nCols, int i, int j) {
 
     for (int k = 0; k < nCols; k++) {
-        double* locRowI = matrix.data() + (k * nRows + i);
-        double* locRowJ = matrix.data() + (k * nRows + j);
-
-        double valRowI = locRowI[0];
-        double valRowJ = locRowJ[0];
-
-        locRowI[0] = valRowJ;
-        locRowJ[0] = valRowI;
+        std::swap(matrix[k * nRows +i], matrix[k * nRows + j]);
     }
 }
 
@@ -23,13 +16,7 @@ void swapCols(std::vector<double> &matrix, int nRows, int nCols, int i, int j) {
     double* locColI = matrix.data() + (i * nRows);
     double* locColJ = matrix.data() + (j * nRows);
 
-    std::vector<double> valColI(locColI, locColI + nRows);
-    std::vector<double> valColJ(locColJ, locColJ + nRows);
-
-    for (int i = 0; i < nRows; i++) {
-        locColI[i] = valColJ[i];
-        locColJ[i] = valColI[i];
-    }
+    std::swap_ranges(locColI, locColI + nRows, locColJ);
 
 }
 
