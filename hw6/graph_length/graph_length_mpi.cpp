08d2a8c7c90559baba05fab2454d3c4b586c55a9
Alasdair Campbell <alicampbellkhaya@gmail.com>
2026-05-18 12:38:37 -0700

    graph length

---
+++ hw6/graph_length/graph_length_mpi.cpp
@@ -0,0 +1,29 @@
+#include "graph_length.hpp"
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+
+    int ip, np;
+    MPI_Comm_rank(MPI_COMM_WORLD, &ip);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+
+    double a = 1.0;
+    double b = 6.0;
+    int n = 1; // placeholder
+
+    double local_a = a + ip * (b - a) / np;
+    double local_b = a + (ip + 1) * (b - a) / np;
+    int local_n = n / np;
+
+    double local_sum = reimann_sum(local_a, local_b, local_n);
+
+    double global_sum;
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (ip == 0) {
+        std::cout << "Result: " << a << "to" << b << "=" << global_sum << std::endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ hw6/graph_length/graph_length_threads.cpp
@@ -0,0 +1,5 @@
+#include "graph_length.hpp"
+
+int main(int argc, char **argv) {
+    
+}
