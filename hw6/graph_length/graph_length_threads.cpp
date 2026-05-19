f414e0da5de7aba86b22d52cf8f74679d012d992
Alasdair Campbell <alicampbellkhaya@gmail.com>
2026-05-18 21:57:50 -0700

    graph length threads done (apart from placeholders

--- hw6/graph_length/graph_length_threads.cpp
+++ hw6/graph_length/graph_length_threads.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
 
     std::ofstream outFileScale("hw6_graph_length_threads_scale.csv");
 
-    std::cout << "num_threads\t time";
+    std::cout << "num threads\t time" << std::endl;
     if (outFileScale.is_open()) {
         outFileScale << "num_threads, time\n";
     }
@@ -32,6 +32,23 @@ int main(int argc, char* argv[]) {
     }
 
     std::ofstream outFileError{"hw6_graph_length_threads_error.csv"};
+
+    std::cout << "partition points\t error" << std::endl;
+    if (outFileError.is_open()) {
+        outFileError << "partition points, error\n";
+    }
+
+    double true_length = 0; //placeholder
+    const int m_threads = 1; //placeholder
+
+    for (int m = 10; m <= 1e6; m *= 10) {
+        double reimann_sum = parallel_reimann_sum(a, b, m, m_threads);
+        double error = abs(reimann_sum - true_length);
+        std::cout << "partition points: " << m << "error: " << error << std::endl;
+        if (outFileError.is_open()) {
+            outFileError << m << "," << error << "\n";
+        }
+    }
 
     return 0;
 }
