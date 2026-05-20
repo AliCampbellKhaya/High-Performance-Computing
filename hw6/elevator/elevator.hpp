79321e667ecda77b20d7b075625cb43cbd0780e3
Alasdair Campbell <alicampbellkhaya@gmail.com>
2026-05-19 23:12:08 -0700

    think i finished elevator (finally)

--- hw6/elevator/elevator.hpp
+++ hw6/elevator/elevator.hpp
@@ -46,10 +46,11 @@ void elevator(int id) {
     vector<pair<int, int>> passengers; // pairs of (person_id, dest_floor)
 
     while (true) {
-        int p_curr_floor = -1;
+        vector<tuple<int, int, int>> passenger_batch;
 
         {
             unique_lock<mutex> lock(queue_mtx);
+
             cv.wait(lock, [] { 
                 return !global_queue.empty() || num_people_serviced.load() >= npeople;
             });
@@ -58,101 +59,117 @@ void elevator(int id) {
                 break;
             }
 
+            // if (num_people_serviced.load() >= npeople) {
+            //     break;
+            // }
+
+            if (global_queue.empty()) {
+                continue;
+            }
+
             tuple<int, int, int> queue_item = global_queue.front();
             global_queue.pop();
 
             int p_id = get<0>(queue_item);
-            p_curr_floor = get<1>(queue_item);
+            int p_curr_floor = get<1>(queue_item);
             int p_dest_floor = get<2>(queue_item);
 
-            passengers.emplace_back(make_pair(p_id, p_dest_floor));
-            occupancy++;
+            passenger_batch.push_back(make_tuple(p_id, p_curr_floor, p_dest_floor));
 
-            queue<tuple<int, int, int>> temp_queue;
+            queue<tuple<int, int, int>> skipped_passengers;
+            while (passenger_batch.size() < MAX_OCCUPANCY && !global_queue.empty()) {
+                tuple<int, int, int> next_queue_item = global_queue.front();
+                int next_p_id = get<0>(next_queue_item);
+                int next_p_curr_floor = get<1>(next_queue_item);
+                int next_p_dest_floor = get<2>(next_queue_item);
 
-            // Handle multiple riders
-            while (!global_queue.empty() && occupancy < MAX_OCCUPANCY) {
-                queue_item = global_queue.front();
-                global_queue.pop();
-
-                p_id = get<0>(queue_item);
-                p_curr_floor = get<1>(queue_item);
-                p_dest_floor = get<2>(queue_item);
-
-                if (p_curr_floor == elevator_positions[id]) {
-                    passengers.emplace_back(make_pair(p_id, p_dest_floor));
-                    occupancy++;
+                if(next_p_curr_floor == p_curr_floor) {
+                    global_queue.pop();
+                    passenger_batch.push_back(make_tuple(next_p_id, next_p_curr_floor, next_p_dest_floor));
                 }
-                // moves all items from the global queue to the temp queue while maintaing ordering
                 else {
-                    temp_queue.push(queue_item);
+                    global_queue.pop();
+                    skipped_passengers.push(make_tuple(next_p_id, next_p_curr_floor, next_p_dest_floor));
                 }
             }
 
-            // swap global queue (which is now empty) with temp queue
-            while (!global_queue.empty()) {
-                temp_queue.push(global_queue.front());
-                global_queue.pop();
+            while (!skipped_passengers.empty()) {
+                global_queue.push(skipped_passengers.front());
+                skipped_passengers.pop();
             }
-            global_queue = move(temp_queue); // restores global queue before giving up lock
 
-            cv.notify_all();
         }
 
-        if (elevator_positions[id] != p_curr_floor) {
+        int start_floor = get<1>(passenger_batch[0]);
+
+        if (elevator_positions[id] != start_floor) {
             {
-            lock_guard<mutex> lock(cout_mtx);
-            cout << "Elevator " << id << " moving from floor " << elevator_positions[id] << " to floor " << p_curr_floor << endl;
+                lock_guard<mutex> lock(cout_mtx);
+                cout << "Elevator " << id << " moving from floor " << elevator_positions[id] << " to floor " << start_floor << endl;
             }
-
-            this_thread::sleep_for(chrono::milliseconds(abs(elevator_positions[id] - p_curr_floor) * 50));
-            elevator_positions[id] = p_curr_floor;
+            this_thread::sleep_for(chrono::milliseconds(abs(elevator_positions[id] - start_floor) * 50));
+            elevator_positions[id] = start_floor;
+            // {
+            //     lock_guard<mutex> lock(queue_mtx);
+            //     elevator_positions[id] = start_floor;
+            // }
         }
 
-        {
-            lock_guard lock(cout_mtx);
-            for (pair<int, int>& passenger : passengers) {
-                cout << "Person " << passenger.first << " entered elevator " << id << endl;
+        for (tuple<int, int, int>& passenger : passenger_batch) {
+            {
+                lock_guard<mutex> lock(cout_mtx);
+                cout << "Person " << get<0>(passenger) << " entered elevator " << id << endl;
+            }
+            passengers.push_back(make_pair(get<0>(passenger), get<2>(passenger)));
+            occupancy++;
+            global_passengers_serviced[id]++;
+        }
+
+        vector<int> destinations;
+        for (pair<int, int> passenger : passengers) {
+            bool already_seen = false;
+            for (int dest : destinations) {
+                if (dest == passenger.second) {
+                    already_seen = true; 
+                    break;
+                }
+            }
+            if (!already_seen) {
+                destinations.push_back(passenger.second);
             }
         }
 
-        while (!passengers.empty()) {
-            int closest_floor = passengers[0].second;
-            int min_distance = abs(elevator_positions[id] - closest_floor);
-
-            for (pair<int, int>& passenger : passengers) {
-                int distance_to_floor = abs(elevator_positions[id] - passenger.second);
-                if (distance_to_floor < min_distance) {
-                    min_distance = distance_to_floor;
-                    closest_floor = passenger.second;
-                }
+        for (int dest : destinations) {
+            {
+                lock_guard<mutex> locak(cout_mtx);
+                cout << "Elevator " << id << " moving from floor " << elevator_positions[id] << " to floor " << dest << endl;
             }
+            this_thread::sleep_for(chrono::milliseconds(abs(elevator_positions[id] - dest) * 50));
+            elevator_positions[id] = dest;
+            // {
+            //     lock_guard<mutex> lock(queue_mtx);
+            //     elevator_positions[id] = dest;
+            // }
+            
 
-            if (elevator_positions[id] != closest_floor) {
-                {
-                    lock_guard lock(cout_mtx);
-                    cout << "Elevator " << id << " moving from floor" << elevator_positions[id] << " to floor " << closest_floor << endl;
-                }
-                this_thread::sleep_for(chrono::milliseconds(abs(elevator_positions[id] - closest_floor) * 50));
-                elevator_positions[id] = closest_floor;
-            }
-
-            auto passenger_dropoff = passengers.begin();
-            while (passenger_dropoff != passengers.end()) {
-                if (passenger_dropoff->second == elevator_positions[id]) {
+            vector<pair<int, int>> remaining_passengers;
+            for (pair<int, int> passenger : passengers) {
+                if (passenger.second == dest) {
                     {
-                        lock_guard lock(cout_mtx);
-                        cout << "Person " << passenger_dropoff->first << "arrived at floor " << elevator_positions[id] << endl;
+                        lock_guard<mutex> lock(cout_mtx);
+                        cout << "Person " << passenger.first << " arrived at floor " << dest << endl;
                     }
                     occupancy--;
                     num_people_serviced++;
-                    global_passengers_serviced[id]++;
-                    passenger_dropoff = passengers.erase(passenger_dropoff);
-                } else {
-                    passenger_dropoff++;
+                    cv.notify_all();
+                }
+                else {
+                    remaining_passengers.push_back(passenger);
                 }
             }
+            passengers = remaining_passengers;
         }
+
     }
 
     {
@@ -163,11 +180,26 @@ void elevator(int id) {
 }
 
 void person(int id) {
+    /* since I had a bug of getting curr_floor = 41
+    and dest_floor = 17 every time */
+    srand(time(nullptr) ^ (id * 2654435761u));
+
     int curr_floor = rand() % NUM_FLOORS;
     int dest_floor = rand() % NUM_FLOORS;
     while (dest_floor == curr_floor) {
         dest_floor = rand() % NUM_FLOORS;
     }
+
+    // DEBUGGING
+    // int r1 = rand();
+    // int r2 = rand();
+    // int curr_floor = r1 % NUM_FLOORS;
+    // int dest_floor = r2 % NUM_FLOORS;
+
+    // {
+    //     lock_guard<mutex> lock(cout_mtx);
+    //     cout << "DEBUG RANDOM " << id << " r1: " << r1 << " r2: " << r2 << " curr floor " << curr_floor << " dest floor " << dest_floor << endl;
+    // }
 
     {
         lock_guard<mutex> lock(cout_mtx);
@@ -179,7 +211,8 @@ void person(int id) {
         global_queue.push(make_tuple(id, curr_floor, dest_floor));
     }
 
-    cv.notify_all();
+    //cv.notify_all();
+    cv.notify_one();
 }
 
 #endif // ELEVATOR_HPP
